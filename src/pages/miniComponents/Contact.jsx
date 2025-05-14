53ea4b665add1f4035cfdeb523a4145a9a3d7fab
MrSubha420 <codewithsubh2.0@gmail.com>
2025-05-14 11:06:35 +0530

    commit

--- src/pages/miniComponents/Contact.jsx
+++ src/pages/miniComponents/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
     setLoading(true);
     try {
       const { data } = await axios.post(
-        "amit-cackend-7at3838oe-subhadip-mannas-projects.vercel.app/api/v1/message/send",
+        "chartbot-17cko5yqk-subhadip-mannas-projects.vercel.app/api/v1/message/send",
         { senderName, subject, message },
         {
           withCredentials: true,
